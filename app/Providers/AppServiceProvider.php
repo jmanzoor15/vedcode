59302da0862614eaf60cb68bd7874b8918a751be
Muzamil Ahmad <muzamilahmad729@gmail.com>
2021-03-14 21:48:33 +0530

    my first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        //$this->registerPolicies();
         Passport::routes();
         // Passport::tokensCan([
         //     'do-anything' => 'Perform any actions'
